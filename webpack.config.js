7f8f960f9ec887b8e65902a288d33013cc293a4c
SergeyKurshin <mr.kurshin@mail.ru>
2021-06-16 00:32:07 +0800

    First commit(start project)

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const pug = {
+    test: /\.pug$/i,
+    use: [
+        {
+            loader: 'html-loader',
+            options: {
+                sources: false,
+            }
+        },
+        'pug-html-loader'
+    ]
+};
+
+const sass = {
+    test: /\.s[ac]ss$/i,
+    use: [
+        MiniCssExtractPlugin.loader,
+        'css-loader',
+        'sass-loader',
+    ]
+};
+
+
+const config = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [pug, sass]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/index.pug',
+            inject: false
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'bundle.css'
+        })
+    ]
+};
+module.exports = config;
